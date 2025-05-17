b7d6cca6bc3e06f1591231339ce06d1f7dd16871
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-05-17 09:47:57 -0500

    refactor: Cambiar comillas dobles a comillas simples en astro.config.mjs para mantener la consistencia en el estilo de código

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,23 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   i18n: {
-    defaultLocale: "en",
-    locales: ["es", "en"],
+    defaultLocale: 'en',
+    locales: ['es', 'en'],
     routing: {
       prefixDefaultLocale: false,
-      redirectToDefaultLocale: true,
-    },
+      redirectToDefaultLocale: true
+    }
   },
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss()]
   },
+  redirects: {
+    '/linkedin': 'https://www.linkedin.com/in/arbelaeznicolas',
+    '/github': 'https://github.com/nicoarbelaez'
+  }
 });
